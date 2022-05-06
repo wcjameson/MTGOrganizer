9fe42a10949500e71d4273f35c1032cd2d0ab47e
wcjameson <williamjameson90@gmail.com>
2022-05-06 14:56:09 -0700

    add CardList component

---
+++ mtg-organizer/src/components/CardList.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Card from './Card';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { useFirestoreConnect, isLoaded,} from 'react-redux-firebase';
+
+function CardList(props) {
+
+  useFirestoreConnect([
+    { collection: 'cards' }
+  ]);
+
+  const cards = useSelector(state => state.firestore.ordered.cards);
+
+  if (isLoaded(cards)) {
+
+    return(
+      <React.Fragment>
+        {cards.map((card) => {
+          return <Card
+          whenCardClicked={props.onCardSelection}
+          name={card.name}
+          color={card.color} />
+        })}
+      </React.Fragment>
+    )
+  } else {
+    return (
+      <React.Fragment>
+        <h3>Summoning collection...</h3>
+      </React.Fragment>
+    )
+  }
+}
+
+CardList.propTypes = {
+  onCardSelection: PropTypes.func
+}
+
+export default CardList;
