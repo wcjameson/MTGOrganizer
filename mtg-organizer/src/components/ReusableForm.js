591fddea8a8c01fcc93227f841c95c2514e0810c
wcjameson <williamjameson90@gmail.com>
2022-05-06 14:26:44 -0700

    add a reusable form component

---
+++ mtg-organizer/src/components/ReusableForm.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ReusableForm(props) {
+
+  return(
+    <React.Fragment>
+      <form onSubmit={props.formSubmissionHandler}>
+      <input
+          type='text'
+          name='name'
+          placeholder='card name' />
+        <input
+          type='text'
+          name='color'
+          placeholder='card color' />
+          <button type='submit'>{props.buttonText}</button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+ReusableForm.propTypes = {
+  buttonText: PropTypes.string,
+  formSubmissionHandler: PropTypes.func
+
+}
